b76e3a96d01a95857142802eeb0aa7ebab932c34
wangjoshuah <imac@dhcp-210-80.lab.cs.dartmouth.edu>
2014-01-29 03:23:43 -0500

    refreshes!
    okay! now it refreshes!

--- EditorCommunicator.java
+++ EditorCommunicator.java
@@ -10,6 +10,7 @@ public class EditorCommunicator extends Thread {
 	private PrintWriter out;		// to server
 	private BufferedReader in;		// from server
 	protected Editor editor;		// handling communication for
+	private Socket sock; 			// the underlying handler for communication
 
 	/**
 	 * Establishes connection and in/out pair
@@ -23,7 +24,7 @@ public class EditorCommunicator extends Thread {
 		else {
 			System.out.println("connecting to " + serverIP + "...");
 			try {
-				Socket sock = new Socket(serverIP, 4242);
+				sock = new Socket(serverIP, 4242);
 				out = new PrintWriter(sock.getOutputStream(), true);
 				in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
 				System.out.println("...connected");
@@ -49,21 +50,21 @@ public class EditorCommunicator extends Thread {
 			// YOUR CODE HERE
 			// Open the socket with the server, and then the writer and reader
 			System.out.println("connecting...");
-			Socket sock = new Socket("localhost", 4242);
-			PrintWriter out = new PrintWriter(sock.getOutputStream(), true);
-			BufferedReader in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+			sock = new Socket("localhost", 4242);
+			out = new PrintWriter(sock.getOutputStream(), true);
+			in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
 			System.out.println("...connected");
-
+			
 			//Set message handler
-			while (true) {
-				String line;
-				while ((line = in.readLine()) != null) {
-					System.out.println("message received");
-					Message msg = new Message(line);
-					msg.update(editor.getSketch());
-				}
+			String line;
+			while ((line = in.readLine()) != null) {
+				System.out.println("message received");
+				Message msg = new Message(line);
+				msg.update(editor.getSketch());
+				editor.repaint();
 			}
-
+			
+			//set the out line
 		}
 		catch (IOException e) {
 			e.printStackTrace();
@@ -72,30 +73,35 @@ public class EditorCommunicator extends Thread {
 			System.out.println("server hung up");
 		}
 
-
+		
 	}
 
 	String noShape = (null + " " + null + " ");
 	public void doAddAt(int idx, Shape shape) {
-		send("doAddAt " + shape.toString() + " " + idx); //passes a string with 8 items separated by spaces. These 8 items are passed to the Message class and read. This 
+		send("doAddAt " + shape.toString() + " " + idx); //passes a string with 8 items separated by spaces. These 8 items are passed to the Message class and read. This
+		editor.repaint();
 	}
 	public void doAddEnd(Shape shape) {
 		send("doAddEnd " + shape.toString() + " -1"); //action plus shape plus index
+		editor.repaint();
 	}
 	public void doRecolor(int idx, Color c) {
 		send("doRecolor " + editor.getSketch().get(idx).toString() + " " + c.hashCode() + " " + Integer.toString(idx)); 
+		editor.repaint();
 	}
 	public void doMoveTo(int idx, int x1, int y1) {
 		send("doMoveTo " + editor.getSketch().get(idx).toString() + " " + Integer.toString(idx));
+		editor.repaint();
 	}
 	public void doDelete(int idx) {
 		send("doDelete " + editor.getSketch().get(idx).toString() + " "+ Integer.toString(idx));
+		editor.repaint();
 	}
 
+	
 
 
-
-
+	
 
 	// Send editor requests to the server
 	// YOUR CODE HERE (methods for different requests)
